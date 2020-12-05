d28b368168365c2af8315870ef47efadcb9a476c
shtrih <shtrih@hitagi.ru>
2020-12-05 20:09:36 +0400

    Typo

--- WS/Messages/Poll.cs
+++ WS/Messages/Poll.cs
@@ -3,7 +3,7 @@ namespace ChaosIV.WS.Messages
 	public class Poll : IMessage
 	{
 		public string type { get; set; } = "create";
-		public string title { get; set; } = "[ChaosIV] Choose effect";
+		public string title { get; set; } = "[ChaosIV] Choose an effect";
 		public string[] choices { get; set; }
 		public int duration { get; set; } = 60;
 		public bool subscriberMultiplier { get; set; } = false;
